cd1b6b4e7be3c064a836f49754f9840d6e6f5ab1
KSGMn <rlatjsrb789@naver.com>
2024-12-23 00:38:50 +0900

    commit

--- todo_backend/src/main/java/com/todo/todo_backend/TodoBackendApplication.java
+++ todo_backend/src/main/java/com/todo/todo_backend/TodoBackendApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableScheduling // 스케쥴러 활성화
 public class TodoBackendApplication {
 
 	public static void main(String[] args) {
+
+		// .env 파일 로드
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(TodoBackendApplication.class, args);
 	}
 
